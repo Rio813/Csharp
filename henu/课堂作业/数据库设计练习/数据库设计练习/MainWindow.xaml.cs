d72b25b92cb410a5e455d908262d27f83f6b62a2
Rio813 <wmy854811839@gmail.com>
2022-11-12 17:45:47 +0800

    add two projects

---
+++ henu/课堂作业/数据库设计练习/数据库设计练习/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 数据库设计练习
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        A07DbEntities1 context = new A07DbEntities1();
+        public MainWindow()
+        {
+            InitializeComponent();
+            object o = new object();
+            RoutedEventArgs e = new RoutedEventArgs();
+            Button_Click(o, e);
+        }
+        //数据查询
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var q = context.Database.SqlQuery<student>("select * from student");
+            datagrid.ItemsSource = q.ToList();
+        }
+    }
+}
---
+++ henu/课堂作业/文本文件读写/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 文本文件读写
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //读文件
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            string filepath = txtfilepath.Text;
+            //string content = File.ReadAllText(filepath);
+            //txtContent.Text = content;
+            //
+            foreach (var v in File.ReadAllLines(filepath))
+                txtContent.Text += v +"\n";
+        }
+        //写文件
+        private void WriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //覆盖写
+            File.WriteAllText(txtfilepath.Text, txtContent.Text);
+            //追加写
+            //File.AppendAllText(txtfilepath.Text, txtContent.Text);
+
+            //清空输入框
+            txtContent.Text = "";
+        }
+        //选择文件
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            //(1)对象实例化
+            OpenFileDialog ofd = new OpenFileDialog();
+            //添加文件筛选器
+            ofd.Filter = "所有文件|*.*|文本文件|*.txt|word文档|*.docx";
+            //打开对话框获得选择文件的路径
+            if(ofd.ShowDialog()==true)
+            {
+                txtfilepath.Text = ofd.FileName;
+            }
+        }
+               
+           }
+}
